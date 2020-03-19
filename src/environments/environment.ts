b206855eab1b524cdc8a629dd9c85e87540da463
Rami <ra997mi@gmail.com>
2020-03-19 10:19:57 +0300

    remove configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCVqcOLmntz31I9RUGutZVB8BINN0L5_0k',
-    authDomain: 'uninews-66107.firebaseapp.com',
-    databaseURL: 'https://uninews-66107.firebaseio.com',
-    projectId: 'uninews-66107',
-    storageBucket: 'uninews-66107.appspot.com',
-    messagingSenderId: '430744726801',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
   }
 };
